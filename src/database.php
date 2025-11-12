f7e696389733cd6f9cc3c206726829417a17f2bf
Philip Allen <philip@northstage.net>
2025-11-12 12:17:37 +0000

    version 0.1

--- src/database.php
+++ src/database.php
@@ -1,12 +1,12 @@
 <?php
 // Read the database connection parameters from environment variables
-$db_host = getenv('DB_HOST');
-$db_name = getenv('DB_NAME');
-$db_user = getenv('DB_USER');
+$db_host = getenv('DB_HOST'); //$db_host = "localhost";
+$db_name = getenv('DB_NAME'); //$db_name = "gallery";
+$db_user = getenv('DB_USER'); //$db_user = "root";
 
 // Read the password file path from an environment variable
 $password_file_path = getenv('PASSWORD_FILE_PATH');
 
 // Read the password from the file
-$db_pass = trim(file_get_contents($password_file_path));
+$db_pass = trim(file_get_contents($password_file_path)); //$db_pass = "password";
 ?>
